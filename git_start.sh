188f8d4ffd7aacb4e311d6f7382eb1bc57188435
mzamoun <mourad.zamoun@gmail.com>
2016-10-11 14:34:42 +0100

    first com

---
+++ git_start.sh
@@ -0,0 +1,7 @@
+echo "# boottutorial" > README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mzamoun/boottutorial.git
+git push -u origin master
+
